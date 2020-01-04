fd56bd39657a9898c1129b6210c71dd2e28718e8
William White <william.white86@gmail.com>
2020-01-03 19:36:53 -0500

    added heroku login creds

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,24 @@
 const mysql = require("mysql")
 require('dotenv').config()
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: process.env.DB_USER,
-    password: process.env.DB_PW,
-    database: 'burgers_db'
-})
+const connection 
+
+// = mysql.createConnection({
+//     host: 'localhost',
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PW,
+//     database: 'burgers_db'
+// })
+
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: process.env.DB_USER,
+        password: process.env.DB_PW,
+        database: 'burgers_db'
+    })
+}
 
 connection.connect();
 
