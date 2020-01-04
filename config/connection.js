68a17a5e0b499a99f76d5ab394a54ae146ead1e0
William White <william.white86@gmail.com>
2020-01-03 19:37:17 -0500

    removed commented code

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,6 @@ const mysql = require("mysql")
 require('dotenv').config()
 const connection 
 
-// = mysql.createConnection({
-//     host: 'localhost',
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PW,
-//     database: 'burgers_db'
-// })
-
 if (process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
